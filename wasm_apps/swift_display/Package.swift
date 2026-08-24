097808b498a3bef9d3eed25e686628d0281084f3
Thomas Karpiniec <tom@banksiasystems.com.au>
2026-08-24 15:09:20 +1000

    Fix Swift display linker settings

--- wasm_apps/swift_display/Package.swift
+++ wasm_apps/swift_display/Package.swift
@@ -18,11 +18,15 @@ let package = Package(
             ],
             linkerSettings: [
                 .unsafeFlags([
-                    "-Xclang-linker", "-nostdlib",
+                    "-Xclang-linker", "-nostartfiles",
                     "-Xlinker", "--no-entry",
                     "-Xlinker", "--export=_start",
                     "-Xlinker", "--allow-undefined",
                     "-Xlinker", "--initial-memory=65536",
+                    "-Xlinker", "--table-base=1",
+                    "-Xlinker", "--strip-all",
+                    "-Xlinker", "--export=malloc",
+                    "-Xlinker", "--export=free",
                     "-Xlinker", "-z", "-Xlinker", "stack-size=8192",
                 ])
             ]
